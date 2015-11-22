97ff7f51c900fd9f52513e4a0361d35346524bb6
matthew weintrub <heytrub@gmail.com>
2015-11-21 22:10:28 -0800

    first commit: layout storyboard

---
+++ chat/InboxViewController.m
@@ -0,0 +1,39 @@
+//
+//  InboxViewController.m
+//  chat
+//
+//  Created by Matthew Weintrub on 11/21/15.
+//  Copyright © 2015 matthew weintrub. All rights reserved.
+//
+
+#import "InboxViewController.h"
+
+@interface InboxViewController ()
+
+@end
+
+@implementation InboxViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self performSegueWithIdentifier:@"showLogin" sender:self];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Incomplete implementation, return the number of sections
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete implementation, return the number of rows
+    return 0;
+}
+
+@end
