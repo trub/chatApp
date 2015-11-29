6637384b8efbc3b3444f9dac0cf9602dd6a96f67
matthew weintrub <heytrub@gmail.com>
2015-11-29 15:21:59 -0800

    makes friends list update in table view after editing

--- chat/FriendsViewController.m
+++ chat/FriendsViewController.m
@@ -19,6 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.friendsRelation = [[PFUser currentUser] objectForKey:@"friendsRelation"];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
     PFQuery *query = [self.friendsRelation query];
     [query orderByAscending:@"username"];
     [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
@@ -30,7 +35,6 @@
             [self.tableView reloadData];
         }
     }];
-    
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
